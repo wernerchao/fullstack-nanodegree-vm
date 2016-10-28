4ec9bce53796b46fe969b8e28c230b98ea23d1fb
Werner Chao <oahcrenrew@gmail.com>
2016-10-28 22:42:00 +0800

    update

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,12 +3,17 @@
 # 
 
 import time
+import psycopg2
 
 ## Database connection
-DB = []
+# DB = []
 
 ## Get posts from database.
 def GetAllPosts():
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute("SELECT content, time FROM posts ORDER BY time DESC")
+
     '''Get all the posts from the database, sorted with the newest first.
 
     Returns:
@@ -16,8 +21,9 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in c.fetchall()]
+    # posts.sort(key=lambda row: row['time'], reverse=True)
+    DB.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +33,10 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute("INSERT INTO posts (content) VALUES (%s)", (content,))
+    DB.commit()
+    DB.close()
+    # t = time.strftime('%c', time.localtime())
+    # DB.append((t, content))
